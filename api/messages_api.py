0a70938d5f66bddd47b66be173a6350cadd6c5b4
Alberto Barrago <albertobarrago@gmail.com>
2024-10-14 18:01:08 +0200

    Removed annoying comments

--- api/messages_api.py
+++ api/messages_api.py
@@ -17,11 +17,9 @@ async def send_message(request: Request, current_user: User = Depends(get_curren
     message_content = form.get("message")
     message = Message(sender=current_user, content=message_content)
 
-    # Send message and save it to persistence layer
-    #send_message_to_network(message_content)
-    # save_message(message)
+    send_message_to_network(message_content)
+    save_message(message, user=current_user)
 
-    # HTML response for the message
     return (f'<li class="bg-white border border-gray-200 rounded-lg p-2 '
             f'shadow-sm transition duration-300 ease-in-out hover:shadow-md">'
             f'<strong>{current_user}:</strong> {message.content} <em>{message.timestamp}</em></li>')
@@ -30,16 +28,13 @@ async def send_message(request: Request, current_user: User = Depends(get_curren
 # GET /history endpoint
 @router.get("/history", response_class=HTMLResponse)
 async def get_history(current_user: User = Depends(get_current_user)):
-    # Initialize ChatService with the logged-in user
     chat_service = ChatService(current_user)
     messages = chat_service.load_history()
 
-    # Construct HTML response for message history
     list_items = "".join(f"<li>{msg}</li>" for msg in messages)
     response_html = f"<ul>{list_items}</ul>".strip().replace('"', ' ')
     return HTMLResponse(content=response_html)
 
 
-# Function to start FastAPI app
 def start_fastapi():
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
